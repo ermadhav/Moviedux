0f469648e2c5d9b00516704472864a413da7395b
ermadhav <madhavtiwari2024@gmail.com>
2025-02-03 00:41:09 +0530

    Nothing much

---
+++ src/components/MoviesGrid.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import "../styles.css";
+
+
+export default function MoviesGrid(){
+
+    const [movies, setMovies] = useState([]);
+
+    return(
+        <div></div>
+    );
+};
