d92aa2fcffa6e92403ae5f0cc312e19da7e56098
ermadhav <madhavtiwari2024@gmail.com>
2025-02-04 23:33:03 +0530

    n

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "../styles.css";
 
-export default function MovieCard(){
-    return(
-        <div></div>
-    );
-};
+export default function MovieCard({movie}) {
+  return (
+    <div>
+      <div key={movie.id} className="movie-card">
+        <img src={`images/${movie.image}`} alt={movie.title} />
+        <div className="movie-card-info">
+          <h3 className="movie-card-title">{movie.title}</h3>
+          <p className="movie-card-genre">{movie.genre}</p>
+          <p className="movie-card-rating">{movie.rating}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
