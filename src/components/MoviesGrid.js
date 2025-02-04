b0802ba03f736dc081b50e1d032b518fef76c4ad
ermadhav <madhavtiwari2024@gmail.com>
2025-02-04 23:56:11 +0530

    nothing

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -2,23 +2,23 @@ import { useState, useEffect } from "react";
 import "../styles.css";
 import MovieCard from "./MovieCard";
 
+export default function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = setState("");
 
-export default function MoviesGrid(){
-
-    const [movies, setMovies] = useState([]);
-    
-    useEffect(() => {
-        fetch("movies.json")
-        .then(response => response.json())
-        .then(data => setMovies(data))
-    }, []);
-    return(
-        <div className="movies-grid">
-            {
-                movies.map(movie => (
-                    <MovieCard movie={movie} key={movie.id}></MovieCard>
-                ))
-            }
-        </div>
-    );
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+  return (
+    <div>
+        <input type="text" placeholder="Search movies..." />
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
 }
