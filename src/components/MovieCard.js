c96f3a44ce3ece40cad128169414f08bf9bff074
ermadhav <madhavtiwari2024@gmail.com>
2025-02-05 18:23:44 +0530

    Some Updation

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -29,10 +29,25 @@ export default function MovieCard({ movie, isWatchlisted, toggleWatchlist }) {
           <h3 className="movie-card-title">{movie.title}</h3>
           <div>
             <span className="movie-card-genre">{movie.genre}</span>
-            <span className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+            <span
+              className={`movie-card-rating ${getRatingClass(movie.rating)}`}
+            >
               {movie.rating}
             </span>
           </div>
+          <label className="switch">
+            <input
+              type="checkbox"
+              checked={isWatchlisted}
+              onChange={() => toggleWatchlist(movie.id)}
+            ></input>
+
+            <span className="slider">
+              <span className="slider-label">
+                {isWatchlisted ? "In Watchlist" : "Add to Watchlist"}{" "}
+              </span>
+            </span>
+          </label>
         </div>
       </div>
     </div>
